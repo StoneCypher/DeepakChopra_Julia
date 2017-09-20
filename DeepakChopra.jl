00637e283ddc74924b9706c4f264a153c8126b69
John Haugeland <stonecypher@gmail.com>
2017-09-19 21:37:49 -0700

    Update DeepakChopra.jl

--- DeepakChopra.jl
+++ DeepakChopra.jl
@@ -1,5 +1,5 @@
 
-print(   rand(["Experiential truth ","The physical world ","Non-judgment ","Quantum physics "]) )
-print(   rand(["nurtures an ","projects onto ","imparts reality to ","constructs with "]) )
-print(   rand(["abundance of ","the barrier of ","self-righteous ","potential "]) )
-println( rand(["marvel.", "choices.", "creativity.", "actions."]) )
+print(   rand( ["Experiential truth ", "The physical world ", "Non-judgment ",       "Quantum physics " ]) )
+print(   rand( ["nurtures an ",        "projects onto ",      "imparts reality to ", "constructs with " ]) )
+print(   rand( ["abundance of ",       "the barrier of ",     "self-righteous ",     "potential "       ]) )
+println( rand( ["marvel.",             "choices.",            "creativity.",         "actions."         ]) )
